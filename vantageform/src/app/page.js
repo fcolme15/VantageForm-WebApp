01d0e297a78f13c013eede956b5a38bcdb8835f9
Francisco Colmenero <franciscojcolmenero@outlook.com>
2025-06-01 15:25:08 -0500

    Basic Webapp skeleton

---
+++ vantageform/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-white">
+      <header className="bg-white text-black">Welcome to Vantage Form</header>
+    </div>
+    
+  );
+}
