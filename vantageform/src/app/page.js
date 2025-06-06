a5544fb53b103c11a511d5d0a325fa92bd58683c
Francisco Colmenero <franciscojcolmenero@outlook.com>
2025-06-06 14:48:15 -0500

    Fixed Button border

--- vantageform/src/app/page.js
+++ vantageform/src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Header from "./components/Header";
+import ButtonGradient from "./assets/svg/ButtonGradient";
 
 export default function Home() {
   return (
     <div >
       <Header/>
+      <ButtonGradient/>
     </div>
     
   );
